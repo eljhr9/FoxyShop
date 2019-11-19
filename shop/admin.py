f09a2c4a39c3c74d08fd9dcb45c5c19b102e81dc
illuhich <1234papricka@gmail.com>
2019-11-19 20:51:57 +0200

    add Brand model

--- shop/admin.py
+++ shop/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Brand
 
 class ProductAdmin(admin.ModelAdmin):
-	list_display = ('title', 'text', 'price', 'availability', 'date_added')
+	list_display = ('title', 'text', 'price', 'availability', 'date_added', 'brand')
 	list_display_links = ('title', 'text')
-	search_fields = ('title', 'text', 'availability', 'date_added', )
+	search_fields = ('title', 'text', 'availability', 'date_added', 'brand',)
 
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Brand)
--- shop/models.py
+++ shop/models.py
@@ -1,14 +1,32 @@
+import os
 from django.db import models
+
+# def get_image_path(instance, filename):
+# 	return os.path.join('photos', str(instance.id), filename)
 
 class Product(models.Model):
 	"""Товар публикуемый на сайте"""
+	# image = models.ImageField(upload_to=get_image_path, blank=True, null=True)
 	title = models.CharField(max_length=50, verbose_name='Название')
 	text = models.TextField(max_length=400, verbose_name='Характеристики')
 	price = models.IntegerField(verbose_name='Цена')
 	availability = models.BooleanField(default=True, verbose_name='Наличие')
 	date_added = models.DateTimeField(auto_now_add=True, verbose_name='Был добавлен')
+	brand = models.ForeignKey('Brand', null=True, blank=True, on_delete=models.PROTECT, verbose_name='Производитель')
 
 	class Meta:
 		verbose_name_plural = 'Товар' 
 		verbose_name = 'Товар'
 		ordering = ['price']
+
+class Brand(models.Model):
+	"""Производитель"""
+	name = models.CharField(max_length=20, primary_key=True, db_index=True, verbose_name='Название')
+
+	def __str__(self):
+		return self.name
+
+	class Meta:
+		verbose_name_plural = 'Производители'
+		verbose_name = 'Производитель'
+		ordering = ['name']
