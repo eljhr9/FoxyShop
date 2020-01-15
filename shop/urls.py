0e607a6555ccd3dcc741f695c7783965c65e81f7
illuhich <49656277+illuhich@users.noreply.github.com>
2020-01-15 20:43:55 +0200

    finished setting pagination and start create footer

--- shop/urls.py
+++ shop/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'shop'
 urlpatterns = [
-	path('', views.products, name='products'),
+	path('all/', views.products, name='products'),
 	path('brand/<int:brand_id>/', views.by_brand, name='by_brand'),
 	path('brand/', views.brand, name='brand'),
 	path('<int:product_id>/', views.product, name='product'),
--- shop/views.py
+++ shop/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.http import HttpResponseRedirect
 from .models import Product, Brand
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def index(request):
 	'''домашняя страница магазина'''
@@ -14,13 +14,25 @@ def products(request):
 	'''Отображение всех товаров'''
 	products = Product.objects.all()
 	brands = Brand.objects.all()
-	paginator = Paginator(products, 6, orphans=0, allow_empty_first_page=True)
-	if 'page' in request.GET:
-		page_num = request.GET['page']
-	else:
-		page_num = 1
-	page = paginator.get_page(page_num)
-	context = {'page': page, 'products': page.object_list, 'brands': brands}
+	# paginator = Paginator(products, 9)
+	# if 'page' in request.GET:
+	# 	page_num = request.GET['page']
+	# else:
+	# 	page_num = 1
+	# page = paginator.get_page(page_num)
+	# context = {'brands': brands, 'page': page, 'products': page.object_list}
+	paginator = Paginator(products, 12)
+	page = request.GET.get('page')
+	try:
+		products = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		products = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		products = paginator.page(paginator.num_pages)
+	# return render(request, 'shop/products.html', {"products": products, "brands": brands})
+	context = {'brands': brands, 'products': products}
 	return render(request, 'shop/products.html', context)
 
 def brand(request):
