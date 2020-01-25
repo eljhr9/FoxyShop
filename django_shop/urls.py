007437a5e4483e934041ff86ef158a6cf0b0ee98
illuhich <49656277+illuhich@users.noreply.github.com>
2020-01-25 17:05:50 +0200

    add grappelli

--- django_shop/urls.py
+++ django_shop/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('shop/', include('shop.urls')),
