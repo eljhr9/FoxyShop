cac5d77a979b2e0fef7b3d7876a1040c734588af
illuhich <49656277+illuhich@users.noreply.github.com>
2020-03-28 22:01:23 +0200

    add pika

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -1,4 +1,10 @@
 from .cart import Cart
+from shop.models import Product
 
 def cart(request):
-    return {'cart': Cart(request)}
+    cartitem = request.session.get('cart')
+    try:
+        cartitems = Product.objects.filter(id__in=cartitem)
+    except:
+        cartitems = None
+    return {'cartitems': cartitems, 'cart': Cart(request)}
